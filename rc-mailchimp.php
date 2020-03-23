1cf787480ceb3d5533992f6e1cfa5c80e85ce279
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-03-23 13:06:21 -0400

    define the plugin version number

--- rc-mailchimp.php
+++ rc-mailchimp.php
@@ -7,4 +7,7 @@ Version: 1.1
 Author: Ronan Corr
 Author URI: 
 License: GPLv2
-*/
+
+*///Add a PHP constant to specify an internal version number that will be used
+//   throughout the plugin code:
+define( "VERSION", "1.1" );
