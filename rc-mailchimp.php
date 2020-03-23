a0744c1d7be45bbcb7af152f0daa83f293330585
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-03-23 13:20:38 -0400

    create maintenance class file and include  MailChimp classes

--- rc-mailchimp.php
+++ rc-mailchimp.php
@@ -11,3 +11,6 @@ License: GPLv2
 *///Add a PHP constant to specify an internal version number that will be used
 //   throughout the plugin code:
 define( "VERSION", "1.1" );
+
+// include MailChimp and custom classes
+require_once 'classes/mcMaintenance.php';
