0cc064f98b993595d5f481e6008ca2501c8f3a31
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-03-23 13:04:27 -0400

    intialize plugin details

---
+++ rc-mailchimp.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: RC MailChimp
+Plugin URI: 
+Description: A newsletter signup form that subscribe users directly to RC's MailChimp account
+Version: 1.1
+Author: Ronan Corr
+Author URI: 
+License: GPLv2
+*/
