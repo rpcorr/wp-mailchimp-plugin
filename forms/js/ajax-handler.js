6376f2ffe18929ac62aedc22bdb758a2c23d9882
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-05-26 13:04:37 -0400

    assign input data to variables

--- forms/js/ajax-handler.js
+++ forms/js/ajax-handler.js
@@ -18,5 +18,13 @@ jQuery(document).ready(function ($) {
 
     // add own ajax check as X-Requested-With is not always reliable
     ajax_form_data = ajax_form_data + '&ajaxrequest=true&submit=Sign+Up';
+
+    //assign input data to variables
+    var firstName = jQuery('#firstName').val();
+    var lastName = jQuery('#lastName').val();
+    var email = jQuery('#email').val();
+    var acknowledge = jQuery('#acknowledge').prop('checked') ? jQuery('#acknowledge').val() : '0';
+    var submit = jQuery('#submit').val();
+    
   });
 });
