9fd0cec35ac69ce6d2c21c66c97eb511a9d37db1
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-05-28 16:12:57 -0400

    got the basic ajax request set up

--- forms/js/ajax-handler.js
+++ forms/js/ajax-handler.js
@@ -1,16 +1,41 @@
-jQuery(document).ready(function ($) {
+/*
+     * The file is enqueued from forms/registration-form.php
+*/
+
+jQuery(function () {
   'use strict';
+  jQuery(document).ready(function ($) {
 
-  /**
-   * The file is enqueued from forms/registration-form.php.
-   */
+    console.log("jQuery is ready");
+
+    jQuery('#wait').css('display', 'none'); // hide the spinner
+
+    jQuery(document).ajaxStart(function () {
+      console.log('ajax has started');
+      jQuery("#wait").css("display", "block");
+    });
+
+    jQuery(document).ajaxComplete(function () {
+      console.log('ajax has completed');
+      jQuery("#wait").css("display", "none");
+    });
 
 
-  jQuery('#wait').css('display', 'none'); // hide the spinner
+    jQuery('#registration-form').on('submit', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('form button has been clicked');
+      console.log('ajax_url is ' + ajax_url);
 
-  jQuery('#registration-form').on('submit', function (event) {
-  
+      jQuery.ajax({
+        type: "POST",
+        url: ajax_url
+      });
+
+
+    });
+
+
   });
 
-  
 });
