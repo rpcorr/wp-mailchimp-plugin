f67e11dc91492c8c9786d15e8525dfc0b8e50d4e
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-05-28 15:13:36 -0400

    start jquery script over again

--- forms/js/ajax-handler.js
+++ forms/js/ajax-handler.js
@@ -5,26 +5,12 @@ jQuery(document).ready(function ($) {
    * The file is enqueued from forms/registration-form.php.
    */
 
+
   jQuery('#wait').css('display', 'none'); // hide the spinner
 
-  jQuery('#registration-form').submit(function (event) {
-    event.preventDefault(); // Prevent the default form submit.
-
-    // show the spinner
-    jQuery('#wait').css('display', 'block');
-
-    // serialize the form data
-    var ajax_form_data = $('#registrationForm').serialize();
-
-    // add own ajax check as X-Requested-With is not always reliable
-    ajax_form_data = ajax_form_data + '&ajaxrequest=true&submit=Sign+Up';
-
-    //assign input data to variables
-    var firstName = jQuery('#firstName').val();
-    var lastName = jQuery('#lastName').val();
-    var email = jQuery('#email').val();
-    var acknowledge = jQuery('#acknowledge').prop('checked') ? jQuery('#acknowledge').val() : '0';
-    var submit = jQuery('#submit').val();
-    
+  jQuery('#registration-form').on('submit', function (event) {
+  
   });
+
+  
 });
