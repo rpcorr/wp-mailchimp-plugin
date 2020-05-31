9b25f931ab959b4da88fa596146689cb38953f1e
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-05-31 17:14:37 -0400

    replace the form and ajax call with a working example and will use as a template

--- forms/registration-form.php
+++ forms/registration-form.php
@@ -1,5 +1,194 @@
 <?php
 
+add_shortcode( 'registration_form', 'rcMC_registration_form' );
+
+function rcMC_registration_form() {
+    global $wpdb;
+
+    if ( !empty( $_GET['searchbt'] ) ) {
+        $search_string = $_GET['searchbt'];
+        $search_mode = true;
+    } else {
+        $search_string = "Search...";
+        $search_mode = false;
+    }
+
+    // Prepare query to retreive bugs from database
+    $bug_query = 'select * from ' . $wpdb->get_blog_prefix();
+    $bug_query .= 'ch7_bug_data ';
+    $bug_query .= 'where bug_status = 0 ';
+
+    // Add search string in query if present
+    if ( $search_mode ) {
+        $search_term = '%'. $search_string . '%';
+        $bug_query .= "and bug_title like '%s' ";
+        $bug_query .= "or bug_description like '%s' ";
+    } else {
+        $search_term = '';
+    }
+    
+    $bug_query .= 'ORDER by bug_id DESC';
+    $bug_items = $wpdb->get_results( $wpdb->prepare( $bug_query,
+                                     $search_term, $search_term ),
+                                     ARRAY_A );
+
+    // Prepare output to be returned to replace shortcode
+    $output = '';
+
+    $output .= '<form method="get" id="ch7_bt_search">';
+    $output .= '<div>Search bugs ';
+    $output .= '<input type="text" onfocus="this.value=\'\'" ';
+    $output .= 'value="' . esc_attr( $search_string ) . '" ';
+    $output .= 'name="searchbt" />';
+    $output .= '<input type="submit" value="Search" />';
+    $output .= '</div>';
+    $output .= '</form><br />';
+
+    $output .= '<a class="show_closed_bugs">';
+    $output .= 'Show closed bugs';
+    $output .= '</a>';
+
+    $output .= '<div class="bug_listing">'; 
+    
+    $output .= '<table>';
+    
+    // Check if any bugs were found
+    if ( !empty( $bug_items ) ) {
+        $output .= '<tr><th style="width: 80px">ID</th>';
+        $output .= '<th style="width: 300px">Title / Desc</th>';
+        $output .= '<th>Version</th></tr>';
+
+        // Create row in table for each bug
+        foreach ( $bug_items as $bug_item ) {
+            $output .= '<tr style="background: #FFF">';
+            $output .= '<td>' . $bug_item['bug_id'] . '</td>';
+            $output .= '<td>' . $bug_item['bug_title'] . '</td>';
+            $output .= '<td>' . $bug_item['bug_version'];
+            $output .= '</td></tr>';
+            $output .= '<tr><td></td><td colspan="2">';
+            $output .= $bug_item['bug_description'];
+            $output .= '</td></tr>';
+        }
+    } else {
+        // Message displayed if no bugs are found
+        $output .= '<tr style="background: #FFF">';
+        $output .= '<td colspan=3>No Bugs to Display</td>';
+    }
+
+    $output .= '</table></div><br />';
+
+    $output .= '<script type="text/javascript">';
+    
+    $nonce = wp_create_nonce( 'rcMC_ajax' );
+    
+    $output .= 'function replacecontent ( bug_status )' .
+               '{ jQuery.ajax( { ' .
+               '    type: "POST",' .
+               '    url: ajax_url, ' .
+               '    data: { action: "rcMC_buglist_ajax", ' .
+               '            _ajax_nonce: "' .  $nonce . '", ' .
+               '            bug_status: bug_status }, ' .
+               '    success: function ( data ) {' .
+               '             jQuery(".bug_listing").html( data ); ' .
+               '             }' .
+               '    });' .
+               '};';
+               
+    $output .= 'jQuery( document ).ready( function () {';
+    $output .= 'jQuery(".show_closed_bugs").click( function()
+                                        { replacecontent( 1 ); } ';
+    $output .= ')});';
+    $output .= '</script>';
+
+    // Return data prepared to replace shortcode on page/post
+    return $output;
+}
+
+
+// register a function to add content to the page header
+add_action( 'wp_head', 'rcMC_declare_ajaxurl' );
+
+// implement rcMC_declare_ajaxurl
+function rcMC_declare_ajaxurl() { ?>
+<script type="text/javascript">
+var ajax_url = '<?php echo admin_url( '
+admin - ajax.php ' ); ?>';
+</script>
+
+<?php
+}
+
+// register functions that will be called when AJAX requests are received from public 
+// or logged in users with an action variable set to rcMC_buglist_ajax
+add_action( 'wp_ajax_rcMC_buglist_ajax', 'rcMC_buglist_ajax' );
+add_action( 'wp_ajax_nopriv_rcMC_buglist_ajax', 'rcMC_buglist_ajax' );
+
+// implement rcMC_buglist_ajax function
+function rcMC_buglist_ajax() {
+    check_ajax_referer( 'rcMC_ajax' );
+
+    if ( isset( $_POST['bug_status']) && 
+         is_numeric( $_POST['bug_status'])) {
+             global $wpdb;
+
+             // Prepare query to retrieve bugs from database
+             $bug_query = 'SELECT * from ' . $wpdb->get_blog_prefix();
+             $bug_query .= 'ch7_bug_data where bug_status = ';
+             $bug_query .= intval( $_POST['bug_status'] );
+             $bug_query .= ' ORDER BY bug_id DESC';
+             
+             $bug_items = $wpdb->get_results(
+                 $wpdb->prepare( $bug_query ), ARRAY_A );
+             
+            // Prepare output to be returned to AJAX requestor
+            $output = '<div class="bug_listing"><table>';
+
+            // Check if any bugs were found
+            if ( $bug_items ) {
+                $output .= '<tr><th style="width:80px">ID</th>';
+                $output .= '<th style="width: 300px">';
+                $output .= 'Title / Desc </th><th>Version</th></tr>'; 
+
+                // Create a row in table for each bug
+                foreach ( $bug_items as $bug_item ) {
+                    $output .= '<tr style="background: #FFF">';
+                    $output .= '<td>' . $bug_item['bug_id'] . '</td>';
+                    $output .= '<td>' . $bug_item['bug_title'] . '</td>';
+                    $output .= '<td>' . $bug_item['bug_version'];
+                    $output .= '</td></tr>';
+                    $output .= '<tr><td colspan="2">';
+                    $output .= $bug_item['bug_description'];
+                    $output .= '</td></tr>';
+                }
+            } else {
+                // Message displayed if no bugs are found
+                $output .= '<tr style="background: #FFF">';
+                $output .= '<td colspan="3">No Bugs to Display</td></tr>';
+            }
+            
+            $output .= '</table></div><br />';
+
+            echo $output;
+         }
+
+         die();
+}
+
+// register a function to be called when scripts are being queued up
+add_action( 'wp_enqueue_scripts', 'rcMC_load_jquery' );
+
+// implement rcMC_load_jquery
+function rcMC_load_jquery() {
+    wp_enqueue_script( 'jquery' );
+}
+
+
+
+/*
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 add_shortcode( 'registration_form', 'registration_form' );
 
 function registration_form() { 
@@ -115,3 +304,5 @@ add_action( 'wp_enqueue_scripts', 'rcMC_load_jquery' );
 function rcMC_load_jquery() {
     wp_enqueue_script( 'jquery' );
 }
+
+*/
