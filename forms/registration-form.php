048eb72adad1619c8eaa7e9f7072680f53b85578
Ronan Corr <3459628+rpcorr@users.noreply.github.com>
2020-05-31 17:22:55 -0400

    replace search form with register form

--- forms/registration-form.php
+++ forms/registration-form.php
@@ -35,13 +35,28 @@ function rcMC_registration_form() {
     // Prepare output to be returned to replace shortcode
     $output = '';
 
-    $output .= '<form method="get" id="ch7_bt_search">';
-    $output .= '<div>Search bugs ';
-    $output .= '<input type="text" onfocus="this.value=\'\'" ';
-    $output .= 'value="' . esc_attr( $search_string ) . '" ';
-    $output .= 'name="searchbt" />';
-    $output .= '<input type="submit" value="Search" />';
+    $output .= '<form method="get" id="registration-form">';
+
+    $output .= '<h2>Be the first to know!</h2>';
+    $output .= '<p>Join our mailing list to receive the latest news.</p>';
+    $output .= '<div style="margin-bottom:15px;">';
+    $output .= '   <label for="firstName">First Name</label>';
+    $output .= '   <input type="text" name="firstName" id="firstName" placeholder="First Name">';
     $output .= '</div>';
+    $output .= '<div style="margin-bottom:15px;">';
+    $output .= '   <label for="lastName">Last Name</label>';
+    $output .= '   <input type="text" name="lastName" id="lastName" placeholder="Last Name">';
+    $outout .= '</div>';
+    $output .= '<div style="margin-bottom:15px;">';
+    $output .= '   <label for="email">Email Address</label>';
+    $output .= '   <input type="text" name="email" id="email" placeholder="name@domain.com">';
+    $output .= '</div>';
+    $output .= '<div style="margin-bottom:15px;">';
+    $output .= '   <input type="checkbox" name="acknowledge" id="acknowledge" style="float:left; margin-top: 5px;">';
+    $output .= '   <label for="acknowledge">I have read and understand the <a href="#">terms of use</a> and <a href="#">privacy
+                policy</a></label>';
+    $output .= '</div>';
+    $output .= '<input type="submit" name="submit" id="submit" value="Sign up">';
     $output .= '</form><br />';
 
     $output .= '<a class="show_closed_bugs">';
